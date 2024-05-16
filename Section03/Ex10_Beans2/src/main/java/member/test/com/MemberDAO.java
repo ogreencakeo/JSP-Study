8f9849368ef1b940fb52ccc2819d48855183b628
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-05-17 07:00:53 +0900

    Create MemberDAO.java

---
+++ Section03/Ex10_Beans2/src/main/java/member/test/com/MemberDAO.java
@@ -0,0 +1,25 @@
+package member.test.com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO {
+	// ArrayList (데이터베이스 이용)
+	
+	List<MemberVO> memberList = new ArrayList<>();
+	
+	// 추가 : add
+	public void add(MemberVO m) {
+		memberList.add(m);
+	}
+	
+	// 출력 : getList()
+	public List<MemberVO> getList(){
+		return memberList;
+	}
+	
+	// 사이즈 : getSize()
+	public int getSize() {
+		return memberList.size();
+	}
+}
