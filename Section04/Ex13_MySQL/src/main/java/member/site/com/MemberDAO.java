982cff533ed35cd100b6546227b1fdf7f6beff53
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-06-04 22:28:41 +0900

    Update MemberDAO.java

--- Section04/Ex13_MySQL/src/main/java/member/site/com/MemberDAO.java
+++ Section04/Ex13_MySQL/src/main/java/member/site/com/MemberDAO.java
@@ -1,5 +1,32 @@
 package member.site.com;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class MemberDAO {
+	// Connection 변수 선언
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	// MySQL 데이터베이스 연결 정보
+	String dbDriver = "com.mysql.cj.jdbc.Driver";
+	String dbUrl = "jdbc:mysql://localhost:3306/mydb2";
+	String dbID ="root";
+	String dbPW = "1234";
+	
+	// 연결 : connect()
+	
+	
+	// 연결 해제 : disconnect()
+	
+	// 입력 : insertDB()
+	
+	// 출력 : listDB()
+	
+	// 수정 : updateDB()
+	
+	// 삭제 : deleteDB()
 	
 }
