b2087a5aa9b0e8d184a742b8692f1c75a51b68d5
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-06 20:19:49 +0900

    Create loginSQL.sql

---
+++ Section05/loginSQL.sql
@@ -0,0 +1,14 @@
+use mydb2;
+
+show tables;
+select * from tbl_member;
+
+insert into tbl_member(id, pw, name) values ("test1", "1234", "테스트1");
+insert into tbl_member(id, pw, name) values ("test2", password("1234"), "테스트1");
+insert into tbl_member(id, pw, name) values ("test3", sha2("1234", 256), "테스트1");
+
+desc tbl_member;
+
+-- 필드 정보 변환하기 
+alter table tbl_member change pw pw varchar(64);
+insert into tbl_member(id, pw, name) values ("test2", sha2("1234", 256), "테스트1");
