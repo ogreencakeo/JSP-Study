3f0a2866267ea935de0c501babf06a4c6d914eb4
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-06 20:51:42 +0900

    Update loginSQL.sql

--- Section05/loginSQL.sql
+++ Section05/loginSQL.sql
@@ -15,3 +15,16 @@ insert into tbl_member(id, pw, name) values ("test2", sha2("1234", 256), "테스
 
 alter table tbl_member change pw password varchar(128);
 alter table tbl_member change password pw varchar(64);
+
+insert into tbl_member(id, pw, name) values ('ironman', sha2('1234', 256), '아이언맨');
+
+-- 데이터 모두 삭제
+delete from tbl_member;
+
+insert into tbl_member(id, pw, name) values('superman', '1234', '슈퍼맨');
+insert into tbl_member(id, pw, name) values('test1', sha2('1234', 256), '테스트1');
+insert into tbl_member(id, pw, name) values('test2', sha2('abcd', 256), '테스트2');
+insert into tbl_member(id, pw, name) values('test3', sha2('abcd1234', 256), '테스트3');
+
+insert into tbl_member(id, pw, name) values('test4', sha2('ab', 256), '테스트4');
+insert into tbl_member(id, pw, name) values('test5', sha2('0123456789abc', 256), '테스트5');
