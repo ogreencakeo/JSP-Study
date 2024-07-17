78d939d78462ea73db28765a0c7be760ed112e14
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-17 22:38:03 +0900

    Update tpl_insert.js

--- Section04/Ex13_MySQL/src/main/webapp/js/tpl_insert.js
+++ Section04/Ex13_MySQL/src/main/webapp/js/tpl_insert.js
@@ -1,8 +1,24 @@
 
-function popUpWindow(){
-	alert("Hello, Korea");	
+function popUpWindow(id, name, url){
+	
+	const chkBox = document.querySelector("#" + id);
+	
+	// 체크박스 체크했는지 확인
+	if(chkBox.checked){
+		// ID 값 추출
+		const idValue = document.querySelector("input[name='id']").value;
+		
+		url = url + "?id=" + idValue ;
+		alert(idValue + "아이디를 중복 체크합니다.");
+	}
+	
+	window.open(url, name, "width=500, height=300, top=0, left=0");
 }
 
 document.querySelector("#chk1").addEventListener("click", function(){
-	popUpWindow();
+	popUpWindow(
+		"chk1", 
+		"chkId", 
+		"http://localhost/Ex13_MySQL/action/popUpWindow_chkId.jsp"
+	);
 });
