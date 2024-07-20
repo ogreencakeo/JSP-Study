e32de4fdb159e583616a3aa7de7d09a620f61e10
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-20 23:56:39 +0900

    Update MemberVO.java

--- Section06/Ex16_Paging/src/main/java/member/site/com/MemberVO.java
+++ Section06/Ex16_Paging/src/main/java/member/site/com/MemberVO.java
@@ -1,5 +1,64 @@
 package member.site.com;
 
 public class MemberVO {
+	// Field
+	private int idx;
+	private String m_id;
+	private String m_pw;
+	private String m_name;
+	private String m_email;
+	private String m_phone;
+	private String m_level;
+	
+	// Getter and Setter
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_pw() {
+		return m_pw;
+	}
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_email() {
+		return m_email;
+	}
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
+	}
+	public String getM_phone() {
+		return m_phone;
+	}
+	public void setM_phone(String m_phone) {
+		this.m_phone = m_phone;
+	}
+	public String getM_level() {
+		return m_level;
+	}
+	public void setM_level(String m_level) {
+		this.m_level = m_level;
+	}
+	
+	// toString() 메서드 재정의
+	@Override
+	public String toString() {
+		return "MemberVO [idx=" + idx + ", m_id=" + m_id + ", m_pw=" + m_pw + ", m_name=" + m_name + ", m_email="
+				+ m_email + ", m_phone=" + m_phone + ", m_level=" + m_level + "]";
+	}
 
 }
