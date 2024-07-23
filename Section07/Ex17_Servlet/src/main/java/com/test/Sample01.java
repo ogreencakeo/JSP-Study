3a0d88eaa5585adb2d61bffcb0d7144dbb95f060
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-24 03:43:25 +0900

    Update Sample01.java

--- Section07/Ex17_Servlet/src/main/java/com/test/Sample01.java
+++ Section07/Ex17_Servlet/src/main/java/com/test/Sample01.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "*.do", loadOnStartup = 1)
+@WebServlet(urlPatterns = "*.go", loadOnStartup = 1)
 public class Sample01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
   
