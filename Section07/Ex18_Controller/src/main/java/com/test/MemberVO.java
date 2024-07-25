dd8429e031385011b7ddd595359cdfdc49069d56
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-26 01:05:21 +0900

    MemberVO.java

--- Section07/Ex18_Controller/src/main/java/com/test/MemberVO.java
+++ Section07/Ex18_Controller/src/main/java/com/test/MemberVO.java
@@ -1,5 +1,49 @@
 package com.test;
 
 public class MemberVO {
-
+	// Field
+	private String id;
+	private String pw;
+	private String name;
+	private String email;
+	private String phone;
+	
+	// Getter and Setter
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	// toString() 재정의
+	@Override
+	public String toString() {
+		return "MemberVO [id=" + id + ", pw=" + pw + ", name=" + name + ", email=" + email + ", phone=" + phone + "]";
+	}
+	
 }
