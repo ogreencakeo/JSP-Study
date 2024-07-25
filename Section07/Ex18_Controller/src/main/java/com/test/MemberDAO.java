e1a27ec8bb564145f4c1a1673596dfa099c4a4ef
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-26 01:05:10 +0900

    MemberDAO.java

--- Section07/Ex18_Controller/src/main/java/com/test/MemberDAO.java
+++ Section07/Ex18_Controller/src/main/java/com/test/MemberDAO.java
@@ -1,5 +1,24 @@
 package com.test;
 
-public class MemberDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MemberDAO {
+	// ArrayList (데이터베이스 역할)
+	List<MemberVO> memberList = new ArrayList<>();
+	
+	// 입력 : add() => /memberInsert.do
+	public void add (MemberVO m) {
+		memberList.add(m);
+	}
+	
+	// 출력 : listDB() => /memberList.do
+	public List<MemberVO> listDB(){
+		return memberList;
+	}
+	
+	// 사이즈 : getSize()
+	public int getSize() {
+		return memberList.size();
+	}
 }
