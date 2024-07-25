2be8371134e4e1a66891b41ef0a0032e05c4fca0
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-26 00:44:58 +0900

    MemberController.class

---
+++ Section07/Ex18_Controller/src/main/java/com/test/MemberController.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class MemberController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public MemberController() {
+        super(); 
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- Section07/Ex18_Controller/src/main/webapp/index.jsp
+++ Section07/Ex18_Controller/src/main/webapp/index.jsp
@@ -1,4 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 
- 
+<%
+	String path = request.getContextPath();
+	out.println(path); // http://localhost/Ex18_Controller/ => /Ex18_Controller
+			
+	// 리다이렉트
+	response.sendRedirect(path + "/memberList.do");
+%>
