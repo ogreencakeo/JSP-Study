7b3cd36d1547ce2b9f5804f65bb3c304d095877b
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-26 01:38:58 +0900

    MemberController.java

--- Section07/Ex18_Controller/src/main/java/com/test/MemberController.java
+++ Section07/Ex18_Controller/src/main/java/com/test/MemberController.java
@@ -1,6 +1,9 @@
 package com.test;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,20 +14,60 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("*.do")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
+	MemberService meberService;
+	MemberVO memberVO;
+	
     public MemberController() {
         super(); 
     }
 
-	public void init(ServletConfig config) throws ServletException {
+	public void init() throws ServletException {
+		meberService = new MemberService();
+		memberVO = new MemberVO();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doHandle(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		doHandle(request, response);
+	}
+	
+	// doGet(), doPost() 요청 모두 여기에서 처리
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 뷰페이지 템플릿
+		String viewPage = "";
+		
+		// 설정
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String action = "/memberInsert.do";
+		
+		// 분기 처리
+		try {
+			if(action.equals("/memberList.do")) {
+				// MemberService.list() 메서드 호출
+				List<MemberVO> dataList = meberService.list();
+				System.out.println(dataList); // []
+			}else if(action.equals("/memberInsert.do")) {
+				// System.out.println(memberVO);
+				// 회원 한 명 입력
+				memberVO.setId("슈퍼맨");
+				memberVO.setPw("1234");
+				memberVO.setName("superman");
+				memberVO.setEmail("superman@gmail.com");
+				memberVO.setPhone("010-1111-1111");
+				
+				// 브라우저 화면에 출력
+				PrintWriter out = response.getWriter();
+				out.println(memberVO);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
