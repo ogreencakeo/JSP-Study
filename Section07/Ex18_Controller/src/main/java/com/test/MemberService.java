6a5a68d6d9965fd158caaee1e0d76515acc313b2
최지민 <116356522+ogreencakeo@users.noreply.github.com>
2024-07-26 01:20:56 +0900

    MemberService.java

--- Section07/Ex18_Controller/src/main/java/com/test/MemberService.java
+++ Section07/Ex18_Controller/src/main/java/com/test/MemberService.java
@@ -1,5 +1,25 @@
 package com.test;
 
-public class MemberService {
+import java.util.List;
 
+public class MemberService {
+	
+	MemberDAO memberDAO;
+	
+	// MemberService() : 생성자를 이용한 MemberDAO 객체 생성
+	public MemberService() {
+		memberDAO = new MemberDAO();
+	}
+	
+	// MemberDAO.add() : /memberInsert.do 요청이 들어왔을 때
+	public void add(MemberVO member) {
+		memberDAO.add(member);
+	}
+	
+	// MemberDAO.listDB() : /memberList.do 요청이 들어왔을 때
+	public List<MemberVO> list(){
+		List<MemberVO> recordList = memberDAO.listDB();
+		
+		return recordList;
+	}
 }
